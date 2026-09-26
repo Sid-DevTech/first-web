17afc5f22c371b31c14f442bcbc58221c17fce0a
Sid-DevTech <siddhesh.sarode12@gmail.com>
2026-09-26 07:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -594,7 +594,7 @@ with col4:
 col1,col2 = st.columns([1,1],gap="medium")
 pages_ui()
 with col1:
-    st.page=("main.py",label="View Full Itinerary", icon=":material/map:",use_container_width=True)
+    st.page=("main.py",title="View Full Itinerary", icon=":material/map:", visibility="visible")
 with col2:
     raw_itinerary= st.session_state.get("itinerary_response")
     itinerary_text= raw_itinerary if raw_itinerary else "No Itinerary Generated yet"
